2f4bb2e41483d058edc49afa7f82f5b39ebe1cad
asiye-cell <kocaasiye520@gmail.com>
2025-11-27 14:32:52 +0300

    profile düzenlendi

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AdminPage from "./pages/AdminPage";
 import BookDetailPage from "./pages/BookDetailPage";
 import Navbar from "./components/Navbar";
 import ProfilePage from "./pages/ProfilePage";
+import NotificaionsPage from "./pages/NotificationsPage";
 
 
 function App() {
@@ -19,10 +20,7 @@ function App() {
              <Route path="/admin" element={<AdminPage />} />
              <Route path="/books/:id" element={<BookDetailPage />} />
              <Route path="/profile" element={<ProfilePage />} />
-
-
-
-
+            <Route path="/notifications" element={<NotificaionsPage />} />
       </Routes>
     </BrowserRouter>
   );
