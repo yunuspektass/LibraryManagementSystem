ac02d6d963fb6412a1d4ebb425109f5607845b00
Ahmet Burak Akbaş <ahmetbrk21@gmail.com>
2025-12-01 00:50:10 +0300

    API endpoints and Gemini Chat bot integration.

--- Core/Domain/BaseEntity.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LibraryManagementSystem.Core.Domain;
-
-public abstract class BaseEntity : ISoftDeletable
-{
-    public int Id { get; set; }
-    public bool Deleted { get; set; } = false;
-}
